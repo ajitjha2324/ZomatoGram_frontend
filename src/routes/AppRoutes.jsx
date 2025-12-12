39fa1bb350a035bc7a9f138c969b97a09c554115
ajitjha2324 <126547032+ajitjha2324@users.noreply.github.com>
2025-12-12 09:02:17 +0530

    userLogin Changes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserRegister from "../pages/auth/UserRegister";
 import ChooseRegister from "../pages/auth/ChooseRegister";
-import UserLogin from "../pages/auth/UserLogin";
+import UserLogin from "../pages/auth/userLogin";
 import FoodPartnerRegister from "../pages/auth/FoodPartnerRegister";
 import FoodPartnerLogin from "../pages/auth/FoodPartnerLogin";
 import Home from "../pages/general/Home";
